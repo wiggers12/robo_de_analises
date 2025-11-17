453bbaeafd01e51d13aad4a22048d03c67766d22
Dionatan wiggers <dionegalato1212@gmail.com>
2025-11-17 16:47:12 -0300

    Update home route to render index.html template
    Changed the response method in the home route to use render_template instead of send_from_directory.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')  # vai procurar em /templates/index.html
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
