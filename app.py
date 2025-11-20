ffd647585cf1d395ac31a24cc33060eefeb46265
Dionatan wiggers <dionegalato1212@gmail.com>
2025-11-19 21:22:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,8 @@ def enviar_whatsapp(destino, mensagem):
 # ==========================
 # START
 # ==========================
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
